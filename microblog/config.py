a3d9ecef385d5f1cf7780075549ed7f8545e60dc
JansenDF <jansen.lima.df@gmail.com>
2022-03-30 17:01:35 -0300

    Finalizado o capitulo 7, porem ate aqui nao consegui receber os emails com os erros

--- microblog/config.py
+++ microblog/config.py
@@ -1,4 +1,5 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -12,5 +13,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
-    
+    ADMINS = ['developer.jansen@gmail.com']
