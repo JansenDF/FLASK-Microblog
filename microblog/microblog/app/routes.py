4bfa15e137e548729ca243170d02f0a7d007401f
JansenDF <jansen.lima.df@gmail.com>
2022-04-13 10:49:01 -0300

    Adicionado recurso de tradção dos post em tempo real

--- microblog/microblog/app/routes.py
+++ microblog/microblog/app/routes.py
@@ -8,9 +8,6 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
     EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
-from langdetect import detect, LangDetectException
-from flask import jsonify
-from app.translate import translate
 
 
 @app.before_request
@@ -27,11 +24,7 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        try:
-            language = detect(form.post.data)
-        except LangDetectException:
-            language = ''
-        post = Post(body=form.post.data, author=current_user, language=language)
+        post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -205,8 +198,3 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
-
-@app.route('/translate', methods=['POST'])
-@login_required
-def translate_text():
-    return jsonify({'text': translate(request.form['text'], request.form['source_language'], request.form['dest_language'])})
